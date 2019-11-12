603608416b242d2aaa5abb8fc67f96b66e896881
Martin Trifonov <mtrifonov@hilscher.com>
2019-11-12 11:48:10 +0200

    fix

--- books_frontend/src/Book.js
+++ books_frontend/src/Book.js
@@ -13,10 +13,10 @@ class Book extends React.Component {
             <span id='square-div'>
                 <React.Fragment>
                     <h2>Name: {this.state.book.name}</h2>
-                    {/* <h3>Author: {this.state.book.author}</h3>
+                    <h3>Author: {this.state.book.author}</h3>
                     <h4>Publisher: {this.state.book.publisher}</h4>
                     <h5>Year: {this.state.book.year}</h5>
-                    <h5>ISBN: {this.state.book.isbn}</h5>*/}
+                    <h5>ISBN: {this.state.book.isbn}</h5>
                     <h6>ID: {this.state.book.id}</h6>
                 </React.Fragment>
             </span>
